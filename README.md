86a71e4cf0d0fa26c89879964d11d712bf9c152a
ihollander <ianholla@gmail.com>
2021-07-11 11:31:31 -0400

    Clarify instructions

--- README.md
+++ README.md
@@ -1,47 +1,120 @@
 # OO Person Lab
 
-![people](https://s3-us-west-2.amazonaws.com/web-dev-readme-photos/oo-labs/people.jpg)
-
 ## Learning Goals
 
 - Gain proficiency instantiating a class
-- Gain ability to discern when to implement `attr_accessor`, `attr_reader`,  and `attr_writer`
-- Gain ability to discern when to define your own `attr_reader` (getter) and `attr_writer` (setter) methods
+- Gain ability to discern when to implement `attr_accessor`, `attr_reader`, and
+  `attr_writer`
+- Gain ability to discern when to define your own `attr_reader` (getter) and
+  `attr_writer` (setter) methods
 
 ## Introduction
 
-To practice object oriented programming (OOP), you're going to create a Person class. Each instance of the `Person` class will have the ability to:
+To practice object oriented programming (OOP), you're going to create a `Person`
+class. Each instance of the `Person` class will have the ability to:
 
-- get paid/receive payments
-- take a bath
-- call a friend
-- start a conversation
-- state if they are happy and/or clean
+- Get paid/receive payments
+- Take a bath
+- Call a friend
+- Start a conversation
+- State if they are happy and/or clean
+
+## A Note on Notation
+
+In the instructions below (and in other labs), you'll see this notation used to
+represent instance methods and class methods:
+
+- `ClassName#method_name`: represents an **instance method** that can be called
+  on the class
+- `ClassName.method_name`: represents a **class method** that can be called on
+  the class
+
+For example, `Person#name` is an example of an instance method, that we can call
+on an instance of a class:
+
+```rb
+class Person
+  def name
+    @name
+  end
+end
+```
+
+We can also define this particular instance method using a macro (which creates
+the getter method for us):
+
+```rb
+class Person
+  attr_reader :name
+
+end
+```
+
+`Person.new` is an example of a class method (remember, to create a new instance
+of the class, we call `ClassName.new`, which then calls the instance method
+`ClassName#initialize`).
 
 ## Instructions
 
-- First you need to create a person class that is initialized with a name that cannot be changed.
-- Each instance of class `Person`should be able to remember their name
-- Each instance of  class `Person` should start with $25 in their bank accounts
-- Each instance of  class `Person` should start with eight happiness points
-- Each instance of  class `Person` should start with eight hygiene points
-- The happiness and hygiene points should be able to change, however the maximum and minimum points for both happiness and hygiene should be 10 and  0 respectively
-- The amount in the bank account should also be able to change, though it has no max or min.
+Create a `Person` class with the following behavior:
 
-##### Non-attribute defining instance methods
+- `Person.new(name)`: takes an argument of a name and saves it to the object.
+  The person's name _should not_ be able to change after initialization. When a
+  new person is created, they should have the following attributes in addition
+  to their name (saved to instance variables):
 
-1. The `clean?` and `happy?` methods are pretty similiar: they should return `true` if the happiness or hygiene points exceed seven. Otherwise they should return false.
+  - `@bank_account` with an initial value of 25
+  - `@happiness` with an initial value of 8
+  - `@hygiene` with an initial value of 8
 
-2. The `get_paid` method should accept a salary amount and add this to the person's bank account. Then the method should return the string "all about the benjamins".
+- `Person#name`: gets the person's name.
 
-3. The `take_bath` method should increment the person's hygiene points by four and return the string "♪ Rub-a-dub just relaxing in the tub ♫".
+- `Person#bank_account`: gets the amount in a person's bank account.
 
-4. The `work_out` method should increment the person's happiness by two points, decrease their hygiene by three points, and return the Queen lyrics, "♪ another one bites the dust ♫".
+- `Person#bank_account=(value)`: sets the value of the bank account to a
+  new amount.
 
-5. The `call_friend` method should accept another instance of the Person class, or "friend". The method should increment the caller and the callee's happiness points by three. If Stella calls her friend Felix, the method should return "Hi Felix! It's Stella. How are you?"
+- `Person#happiness`: gets the value of the person's happiness points.
 
-6. Finally, the `start_conversation` method should accept two arguments, the person to start a conversation with and the topic of conversation.
+- `Person#happiness=(value)`: sets the value of the person's happiness
+  points. The maximum value allowed is 10, and the minimum value allowed is 0.
 
-  - If the topic is politics, both people get sadder and the method returns "blah blah partisan blah lobbyist".
-  - If the topic is weather, both people get a little happier and the method returns "blah blah sun blah rain".
-  - If the topic is not politics or weather, their happiness points don't change and the method returns "blah blah blah blah blah".
+- `Person#hygiene`: gets the value of the person's hygiene points.
+
+- `Person#hygiene=(value)`: sets the value of the person's hygiene
+  points. The maximum value allowed is 10, and the minimum value allowed is 0.
+
+### Additional Instance Methods
+
+- `Person#clean?`: returns `true` if the person's hygiene is more than seven;
+  otherwise, it returns `false`.
+
+- `Person#happy?`: returns `true` if the person's happiness is more than seven;
+  otherwise, it returns `false`.
+
+- `Person#get_paid(amount)`: accepts a salary amount and adds this to the
+  person's bank account. The method should return the string
+  `"all about the benjamins"`.
+
+- `Person#take_bath`: increments the person's hygiene points by four and returns
+  the string `"♪ Rub-a-dub just relaxing in the tub ♫"`.
+
+- `Person#work_out`: increments the person's happiness by two points, decreases
+  their hygiene by three points, and return the Queen lyrics,
+  `"♪ another one bites the dust ♫"`.
+
+- `Person#call_friend(friend)`: accepts another instance of the `Person` class,
+  or "friend". The method should increment the person and the friend's happiness
+  points by three. It should also return a string. For example, if Stella calls
+  her friend Felix, the method increment both Stella and Felix's happiness
+  points and then return `"Hi Felix! It's Stella. How are you?"`.
+
+- `Person#start_conversation(friend, topic)`: accept two arguments, the friend
+  to start a conversation with and the topic of conversation.
+
+  - If the topic is politics, both people get sadder (their happiness decreases
+    by 1) and the method returns `"blah blah partisan blah lobbyist"`.
+  - If the topic is weather, both people get a little happier (their happiness
+    increases by 1) and the method returns `"blah blah sun blah rain"`.
+  - If the topic is not politics or weather, their happiness points don't change
+    and the method returns `"blah blah blah blah blah"`.
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,134 +2,131 @@ RSpec.describe 'Person' do
   let(:stella) { Person.new('Stella') }
   let(:blanche) { Person.new('Blanche') }
   let(:people) { [stella, blanche] }
-  let!(:name_hash) { { stella => 'Stella', blanche => 'Blanche' } }
 
-  context 'instantiation' do
-    describe '::new' do
+  describe 'initialization and attributes' do
+    describe '.new' do
       it 'a new person is instantiated with a name' do
-        expect { Person.new('Blake') }.to_not raise_error
+        expect { Person.new('Blake') }.not_to raise_error
       end
     end
 
-    describe '#initialize' do
-      describe '#name' do
-        it 'a new person knows its name once he/she has been initialized' do
-          name_hash.each do |person, name|
-            expect(person.name).to eq(name)
-          end
-        end
+    describe '#name' do
+      it 'returns the name the person was initialized with' do
+        expect(stella.name).to eq('Stella')
+        expect(blanche.name).to eq('Blanche')
+      end
+    end
 
-        it 'a new person instance cannot overwrite the name it was instantied with' do
-          name_hash.each do |person, _name|
-            expect { person.name = 'some_new_name' }.to raise_error(NoMethodError)
-          end
-        end
+    describe '#name=' do
+      it 'a new person instance cannot overwrite the name they were instantiated with' do
+        expect { stella.name = 'some_new_name' }.to raise_error(NoMethodError)
+      end
+    end
+
+    describe '#bank_account' do
+      it 'a new person instance is initialized with a bank_account balance of $25' do
+        expect(stella.bank_account).to eq(25)
+        expect(blanche.bank_account).to eq(25)
+      end
+    end
+
+    describe '#bank_account=' do
+      it 'allows a person to change their bank account amount' do
+        original_amount = stella.bank_account
+        expect(stella.bank_account += 1).to eq(original_amount + 1)
+      end
+    end
+
+    describe '#happiness' do
+      it 'a new person instance is initialized with a happiness of 8' do
+        expect(stella.happiness).to eq(8)
+        expect(blanche.happiness).to eq(8)
+      end
+    end
+
+    describe '#happiness=' do
+      it 'allows a person to change their happiness' do
+        stella.happiness = 9
+        expect(stella.happiness).to eq(9)
       end
 
-      describe '#bank_account' do
-        it 'a new person instance is initialized with a bank_account balance of $25' do
-          people.each { |person| expect(person.bank_account).to eq(25) }
-        end
-
-        it 'a person instance can change his/her bank_account amount' do
-          people.each do |person|
-            original_amount = person.bank_account
-            expect(person.bank_account += 1).to eq (original_amount + 1)
-          end
-        end
+      it 'does not allow a person to change their happiness above 10' do
+        stella.happiness = 11
+        expect(stella.happiness).to eq(10)
       end
 
-      describe '#happiness' do
-        it ' a new person instance is initialized with a happiness index of 8' do
-          people.each { |person| expect(person.happiness).to eq(8) }
-        end
+      it 'does not allow a person to change their happiness below 0' do
+        stella.happiness = -1
+        expect(stella.happiness).to eq(0)
+      end
+    end
 
-        it 'a person instance can change his/her happiness index' do
-          people.each do |person|
-            person.happiness = 9
-            expect(person.happiness).to eq(9)
-          end
-        end
+    describe '#hygiene' do
+      it 'a new person instance is initialized with a hygiene of 8' do
+        expect(stella.hygiene).to eq(8)
+        expect(blanche.hygiene).to eq(8)
+      end
+    end
 
-        it "a person's happiness doesn't exceed 10" do
-          people.each do |person|
-            person.happiness = 11
-            expect(person.happiness).to eq(10)
-          end
-        end
-
-        it "a person's happiness doesn't go below 0" do
-          people.each do |person|
-            person.happiness = -1
-            expect(person.happiness).to eq(0)
-          end
-        end
+    describe '#hygiene=' do
+      it 'allows a person to change their hygiene' do
+        stella.hygiene = 9
+        expect(stella.hygiene).to eq(9)
       end
 
-      describe '#hygiene' do
-        it 'a person instance is initialized with a hygiene index of 8' do
-          people.each { |person| expect(person.hygiene).to eq(8) }
-        end
+      it 'does not allow a person to change their hygiene above 10' do
+        stella.hygiene = 11
+        expect(stella.hygiene).to eq(10)
+      end
 
-        it 'a person instance can change its hygiene index' do
-          people.each do |person|
-            person.hygiene = 9
-            expect(person.hygiene).to eq(9)
-          end
-        end
-
-        it "a person instance's hygiene index cannot exceed 10 " do
-          people.each do |person|
-            person.hygiene = 11
-            expect(person.hygiene).to eq(10)
-          end
-        end
-
-        it ' a person instance hygiene index cannot be less than 0 ' do
-          people.each do |person|
-            person.hygiene = -1
-            expect(person.hygiene).to eq(0)
-          end
-        end
+      it 'does not allow a person to change their hygiene below 0' do
+        stella.hygiene = -1
+        expect(stella.hygiene).to eq(0)
       end
     end
   end
 
-  context 'non-attribute instance methods' do
+  describe 'non-attribute instance methods' do
     let(:penelope) { Person.new('Penelope') }
     let(:felix) { Person.new('Felix') }
     let(:people) { [penelope, felix] }
 
     describe '#happy?' do
       it 'returns true if happiness is greater than seven, else false' do
-        penelope.happiness = 7
-        expect(penelope.happy?).to eq(false)
         penelope.happiness = 8
         expect(penelope.happy?).to eq(true)
+      end
+
+      it 'returns false if happiness is not greater than seven' do
+        penelope.happiness = 7
+        expect(penelope.happy?).to eq(false)
       end
     end
 
     describe '#clean?' do
-      it 'returns true if hygiene is greater than seven, else false' do
-        penelope.hygiene = 7
-        expect(penelope.clean?).to eq(false)
+      it 'returns true if hygiene is greater than seven' do
         penelope.hygiene = 8
         expect(penelope.clean?).to eq(true)
+      end
+
+      it 'returns false if hygiene is not greater than seven' do
+        penelope.hygiene = 7
+        expect(penelope.clean?).to eq(false)
       end
     end
 
     describe '#get_paid' do
-      it 'accepts an argument of salary' do
-        expect { penelope.get_paid(100) }.to_not raise_error
+      it 'accepts an argument of amount' do
+        expect { penelope.get_paid(100) }.not_to raise_error
       end
 
-      it 'increments bank_account by the salary' do
+      it 'increments bank_account by the amount' do
         original_amount = penelope.bank_account
         penelope.get_paid(100)
-        expect(penelope.bank_account).to eq(original_amount += 100)
+        expect(penelope.bank_account).to eq(original_amount + 100)
       end
 
-      it "returns 'all about the benjamins'" do
+      it 'returns "all about the benjamins"' do
         expect(penelope.get_paid(100)).to eq('all about the benjamins')
       end
     end
@@ -166,11 +163,9 @@ RSpec.describe 'Person' do
         expect(penelope.hygiene).to eq(7)
       end
 
-      it "never makes the person have negative dirty points
-        (hint: use the #hygiene= method)" do
+      it 'never makes the person have negative dirty points (hint: use the #hygiene= method)' do
         penelope.hygiene = 1
         penelope.work_out
-        expect(penelope.hygiene).to be >= 0
         expect(penelope.hygiene).to eq(0)
       end
 
@@ -186,8 +181,7 @@ RSpec.describe 'Person' do
         expect(penelope.happiness).to eq(9)
       end
 
-      it "never makes the person have more than 10 happiness points
-        (hint: use the #happiness= method)" do
+      it 'never makes the person have more than 10 happiness points (hint: use the #happiness= method)' do
         penelope.happiness = 9
         penelope.work_out
         expect(penelope.happiness).to be <= 10
@@ -208,7 +202,7 @@ RSpec.describe 'Person' do
 
     describe '#call_friend' do
       it 'accepts one argument, an instance of the Person class' do
-        expect { penelope.call_friend(felix) }.to_not raise_error
+        expect { penelope.call_friend(felix) }.not_to raise_error
       end
 
       it 'makes the person calling happier by three points' do
@@ -223,38 +217,38 @@ RSpec.describe 'Person' do
         expect(felix.happiness).to eq(8)
       end
 
-      it "never makes the happiness of either party exceed 10
-        (hint: use the cutom #happiness= method)" do
-        [felix, penelope].each { |person| person.happiness = 9 }
+      it 'never makes the happiness of either party exceed 10 (hint: use the custom #happiness= method)' do
+        people.each { |person| person.happiness = 9 }
+        
         penelope.call_friend(felix)
-        [felix, penelope].each do |person|
-          expect(person.happiness <= 10).to eq(true)
+        
+        people.each do |person|
           expect(person.happiness).to eq(10)
         end
       end
 
       it 'calls on the #happiness= method to increment happiness' do
-        [felix, penelope].each { |person| person.happiness = 9 }
+        people.each { |person| person.happiness = 9 }
+        
         people.each do |person|
           expect(person).to receive(:happiness=).with(12).and_return(10)
         end
+        
         penelope.call_friend(felix)
       end
 
       it "returns a string that reflects the caller's side of the conversation" do
-        { felix => penelope, penelope => felix }.each do |caller, callee|
-          convo = "Hi #{callee.name}! It's #{caller.name}. How are you?"
-          expect(caller.call_friend(callee)).to eq(convo)
-        end
+        expect(felix.call_friend(penelope)).to eq("Hi Penelope! It's Felix. How are you?")
+        expect(penelope.call_friend(felix)).to eq("Hi Felix! It's Penelope. How are you?")
       end
     end
 
     describe '#start_conversation' do
       it 'accepts two arguments, a person to strike a convo with and a topic' do
-        expect { penelope.start_conversation(felix, 'politics') }.to_not raise_error
+        expect { penelope.start_conversation(felix, 'politics') }.not_to raise_error
       end
 
-      context 'topic: politics' do
+      context 'with topic: politics' do
         it "returns 'blah blah partisan blah lobbyist' if the topic is politics" do
           convo = 'blah blah partisan blah lobbyist'
           expect(penelope.start_conversation(felix, 'politics')).to eq(convo)
@@ -266,18 +260,16 @@ RSpec.describe 'Person' do
           people.each { |person| expect(person.happiness).to eq(5) }
         end
 
-        it "never makes either party less than 0 points happy (never negative)
-          (hint: use your #happiness= method)" do
+        it 'never makes either party less than 0 points happy (never negative) (hint: use your #happiness= method)' do
           people.each { |person| person.happiness = 1 }
           penelope.start_conversation(felix, 'politics')
           people.each do |person|
-            expect(person.happiness).to be >= 0
             expect(person.happiness).to eq(0)
           end
         end
 
         it 'calls on the #happiness= method' do
-          [felix, penelope].each { |person| person.happiness = 1 }
+          people.each { |person| person.happiness = 1 }
           people.each do |person|
             expect(person).to receive(:happiness=).with(-1).and_return(0)
           end
@@ -285,7 +277,7 @@ RSpec.describe 'Person' do
         end
       end
 
-      context 'topic: weather' do
+      context 'with topic: weather' do
         it "returns 'blah blah sun blah rain' if the topic is weather" do
           convo = 'blah blah sun blah rain'
           expect(penelope.start_conversation(felix, 'weather')).to eq(convo)
@@ -297,18 +289,16 @@ RSpec.describe 'Person' do
           people.each { |p| expect(p.happiness).to eq(8) }
         end
 
-        it "never makes either party more than 10 points happy
-          (hint: use your #happiness= method)" do
+        it 'never makes either party more than 10 points happy (hint: use your #happiness= method)' do
           people.each { |person| person.happiness = 10 }
           penelope.start_conversation(felix, 'weather')
           people.each do |person|
-            expect(person.happiness).to be <= 10
             expect(person.happiness).to eq(10)
           end
         end
 
         it 'calls on the #happiness= method' do
-          [felix, penelope].each { |person| person.happiness = 10 }
+          people.each { |person| person.happiness = 10 }
           people.each do |person|
             expect(person).to receive(:happiness=).with(11).and_return(10)
           end
@@ -316,7 +306,7 @@ RSpec.describe 'Person' do
         end
       end
 
-      context 'topic: other' do
+      context 'with topic: other' do
         it "returns 'blah blah blah blah blah' if the topic is not politics or weather" do
           convo = 'blah blah blah blah blah'
           expect(penelope.start_conversation(felix, 'programming')).to eq(convo)
