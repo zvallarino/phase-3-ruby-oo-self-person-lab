b0f0cdbc85420c6d2f9aab427220670923e1e068
Zachary <zvcoding@gmail.com>
2021-11-03 12:44:48 -0400

    commit tree 0d49e7be8fba0f94838cadb26375bd355f3fba76

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,40 @@
-# your code goes here
+class Person
+
+  attr_reader :name
+  
+  
+
+  attr_accessor  :bank_account, :hygiene
+
+     # setter method
+     def happiness=(happiness)
+      if (happiness <= 10 && happiness >=0)
+      @happiness = happiness
+    end
+  
+    # getter method
+    def happiness
+      @happiness
+    end
+
+  def initialize( name, bankaccount = 25,happiness = 8,hygiene = 8) 
+    @name = name
+    @bank_account = bankaccount
+    @happiness = happiness
+    @hygiene = hygiene
+
+  end
+
+  def getname
+    self.name
+  end 
+
+ 
+  
+
+
+end 
+
+zack = Person.new('Zach')
+p zack
+
